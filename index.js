2dc5a69deb587ce131827dbd62cb178d9650914e
mae-yosores2k19 <jessamaehortadoyosores@gmail.com>
2019-09-18 10:00:53 +0800

    balo_Uie

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
+
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 
-app.get('/', function(req, res){
+
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+
+io.on('connection', function (socket) {
+  socket.on('chat message', function (data) {
+    io.emit('chat message', data);
   });
+  socket.on('online', function (data) {
+    if (!online.includes(data.username)) {
+      online.push(data);
+    }
+    io.emit('online', online);
+  });
+  socket.on('typing', function (data){
+    io.emit('typing',data);
+  })
 });
 
-http.listen(port, function(){
+
+
+
+
+
+http.listen(port, function () {
   console.log('listening on *:' + port);
 });
+
+
