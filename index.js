d2264a7200a4b4250cacac3a410be6d963cddc15
mae-yosores2k19 <jessamaehortadoyosores@gmail.com>
2019-09-19 18:57:13 +0800

    Yol Gwapo! <3

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 8000;
+var active=[];
 
 
 app.get('/', function (req, res) {
@@ -15,15 +16,29 @@ io.on('connection', function (socket) {
   socket.on('chat message', function (data) {
     io.emit('chat message', data);
   });
-  socket.on('online', function (data) {
-    if (!online.includes(data.username)) {
-      online.push(data);
-    }
-    io.emit('online', online);
-  });
-  socket.on('typing', function (data){
-    io.emit('typing',data);
+  
+
+  socket.on('online',function(username){
+    
+    active.push({id : socket.id, username:username})
+    setInterval(function(){
+      socket.emit('online',active);
+    },1000)
   })
+
+
+  socket.on('disconnect',function(user){
+    for (let i = 0; i < active.length; i++) {
+      if(active[i].id == socket.id){
+        active.splice(i,1)
+        break;
+      }
+      
+    }
+  })
+  // socket.on('typing', function (data){
+  //   io.emit('typing',data);
+  // })
 });
 
 
